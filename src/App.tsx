583587fa02e0ca83a18d6aa8526a62114962e2ea
Augusto Camati <63523508+augustocamati@users.noreply.github.com>
2025-08-12 22:42:18 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Courses from "./pages/Courses";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-import Content from "./pages/Content";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,16 +19,17 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/cursos" element={<Courses />} />
-          <Route path="/servicos" element={<Services />} />
-          <Route path="/contacto" element={<Contact />} />
-          <Route path="/conteudos" element={<Content />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/cursos" element={<Courses />} />
+            <Route path="/servicos" element={<Services />} />
+            <Route path="/servicos" element={<Services />} />
+            <Route path="/contacto" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
